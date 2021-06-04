5ddece8c2bd87f0b73cc6ec9e0431484ca16ca9f
Sophie Assemat <sophieassemat81@gmail.com>
2021-06-04 10:25:39 +0200

    add method to read data

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -4,17 +4,11 @@ import java.util.List;
 
 /**
  * Anything that will read symptom data from a source
- * The important part is, the return value from the operation, which is a list of strings,
- * that may contain many duplications
- * 
- * The implementation does not need to order the list
  * 
  */
 public interface ISymptomReader {
-	/**
-	 * If no data is available, return an empty List
-	 * 
-	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
-	 */
-	List<String> GetSymptoms ();
+	// method to read the file
+	public String symptomReader() throws Exception;
+
+	List<String> GetSymptoms();
 }
