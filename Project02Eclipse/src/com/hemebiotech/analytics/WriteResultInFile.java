9856dfc8f50ae7b2a13237d1dfb3d011cf2e5956
Sophie Assemat <sophieassemat81@gmail.com>
2021-06-04 10:39:56 +0200

    add WriteResultInFile class to write the results in a new file

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteResultInFile.java
@@ -0,0 +1,48 @@
+package com.hemebiotech.analytics;
+
+import java.io.FileWriter;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class WriteResultInFile implements ISymptomWriter {
+	// Create and Write a file results.out.txt
+	/**
+	 * 
+	 * Create a new file results.out.txt. Incorporate the results of counting in the
+	 * file. Use iterator to cycle through the collection to obtain the elements
+	 * 
+	 * Write the pairs in the new file separate by "="
+	 * 
+	 * @throws Exception if the file is not create
+	 * 
+	 */
+	public void writeSymptomAndOccurrence(TreeMap<String, Long> counter) {
+
+		try {
+
+			// create the new file
+			FileWriter newfile = new FileWriter("results.out.txt");
+
+			// Incorporate the results in a Set<Map.Entry<String,Long>>
+			Set<Map.Entry<String, Long>> results = counter.entrySet();
+			// Use iterator to cycle through the collection to obtain the elements
+			Iterator<Map.Entry<String, Long>> iter = results.iterator();
+
+			// Use a while loop to browse the keys and view the entries for each key
+			while (iter.hasNext()) {
+				Map.Entry<String, Long> result = (Entry<String, Long>) iter.next();
+
+				// Write the result in the new file
+				newfile.write(result.getKey() + " = " + result.getValue() + "\n");
+				System.out.println(result.getKey() + " = " + result.getValue() + "\n");
+			}
+			newfile.close();
+		} catch (Exception e) {
+		}
+
+	}
+
+}
